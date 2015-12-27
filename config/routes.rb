f8e3f6912dacaff64932a8607101503e4fb26cab
Stefan Wapnick <stefanwapnick@hotmail.com>
2015-12-26 21:10:34 -0500

    Added layout for viewing and searching for user friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resource :friendships
   root 'welcome#index'
 
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search', to: 'stocks#search'
   get 'my_frields', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
+
   devise_for :users, :controllers => { :registrations => 'user/registrations' }
 end
