8e51bfb17889a83b2ff2ef3d2718c3fa0f23c586
Stefan Wapnick <stefanwapnick@hotmail.com>
2015-12-27 00:01:16 -0500

    Updated device users controller to proper subclass

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,11 +1,11 @@
-class RegistrationsController < Devise::RegistrationsController
+class User::RegistrationsController < Devise::RegistrationsController
 
-  before_action :configure_permitted_parameters
+  before_filter :configure_permitted_parameters
 
   protected
-    def configure_permitted_paramters
-      devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
-      devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name)
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
+    devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name)
+  end
 
 end
